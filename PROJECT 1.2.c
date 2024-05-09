17f185b49bb11cded562a6ee256cf3035c40f035
Macy Alberto <116829240+macyjalberto@users.noreply.github.com>
2024-05-09 16:56:15 -0600

    Update PROJECT 1.2.c

--- PROJECT 1.2.c
+++ PROJECT 1.2.c
@@ -2,10 +2,14 @@
    PROJECT: 2
    AUTHOR: MACYJALBERTO
    DATE: MAY 09, 2024
+   PROJECT DESCRIPTION: WRITE PROGRAM COMPUTING VOLUME OF SPHERE WITH 10-METRE RADIUS USING FORMULA v = 4/3πr^3
 */
 #include <stdio.h>
 
+#define PI 3.14159265358979323846
+
 int main(void) {
-    printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
+    float r = 10;
+    printf("The volume of a sphere with a 10-metre radius is: %.2f", PI * 4.0f/3.0f * r * r * r);
     return 0;
 }
