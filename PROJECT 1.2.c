ed18443b94fae4b90a2a320c6ac2f7fc071083b7
Macy Alberto <116829240+macyjalberto@users.noreply.github.com>
2024-05-09 15:56:13 -0600

    Create PROJECT 1.2.c

---
+++ PROJECT 1.2.c
@@ -0,0 +1,11 @@
+/* CHAPTER: 1
+   PROJECT: 2
+   AUTHOR: MACYJALBERTO
+   DATE: MAY 09, 2024
+*/
+#include <stdio.h>
+
+int main(void) {
+    printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
+    return 0;
+}
