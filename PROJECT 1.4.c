b85a5316b5864ead1ef261954bcd0d4aa154ccba
Macy Alberto <116829240+macyjalberto@users.noreply.github.com>
2024-05-09 17:16:42 -0600

    Update PROJECT 1.4.c

--- PROJECT 1.4.c
+++ PROJECT 1.4.c
@@ -2,12 +2,14 @@
    PROJECT: 4
    AUTHOR: MACYJALBERTO
    DATE: MAY 09, 2024
-   PROJECT DESCRIPTION: 
+   PROJECT DESCRIPTION: WRITE PROGRAM ASKING USER TO ENTER DOLLARS-AND-CENTS AMOUNT, THEN DISPLAY THE AMOUNT WITH 5% TAX ADDED
 */
 #include <stdio.h>
 
 int main(void) {
-    float r = 10;
-    printf("The volume of a sphere with a 10-metre radius is: %.2f", PI * 4.0f/3.0f * r * r * r);
+    float amount;
+    printf("Enter an amount: $");
+    scanf("%f", &amount);
+    printf("With tax added: $%.2f", amount * 1.05); // because multiply by 1 gives amount, but multiply by 0.05 gives 5% tax, so 1.05 are those combined together
     return 0;
 }
