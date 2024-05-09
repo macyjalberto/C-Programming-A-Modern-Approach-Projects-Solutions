d34aeede5895ccaeff754cc3f854fdce3045cd86
Macy Alberto <116829240+macyjalberto@users.noreply.github.com>
2024-05-09 15:57:03 -0600

    Update PROJECT 1.1.c

--- PROJECT 1.1.c
+++ PROJECT 1.1.c
@@ -1,10 +1,11 @@
 /* CHAPTER: 1
-    PROJECT: 1
-    AUTHOR: MACYJALBERTO
+   PROJECT: 1
+   AUTHOR: MACYJALBERTO
+   DATE: MAY 09, 2024
 */
 #include <stdio.h>
 
 int main(void) {
-    printf("hi");
+    printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
     return 0;
 }
